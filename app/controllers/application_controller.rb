f91c4d3d1a7569633e13d6bfeac3cbc820381775
sttagami2 <sttagami2@gmail.com>
2020-03-12 15:56:31 +0900

    [Update] App2 devise, Bootstrap, refile, refile-mini_magicを実装。また、sing up, sing in実装。次やることはBookのモデル作成等々

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # ↑ devise利用の機能（ユーザ登録、ログイン認証など）が使われる場合、その前にconfigure_permitted_parametersが実行されます。
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  # ↑　ユーザ登録（sign_up）の際に、ユーザ名（name）のデータ操作が許可されます。
 end
